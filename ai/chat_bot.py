b45f97ce4798a51e7c2b90ec1281988fe6549815
Luiz Fellipe Montibeller <60435968+lzfellipe@users.noreply.github.com>
2020-02-09 17:52:27 +0000

    changes to getting input and showing output from command line

--- ai/chat_bot.py
+++ ai/chat_bot.py
@@ -5,11 +5,14 @@ from data_pre_processor import DataPreProcessor
 # Takes input from commandline, processes it, displays processed output via commandline.
 def main():
     # Where input is user input from commandline.
-    preProcessor = DataPreProcessor(input)
+    print("Enter the input")
+    userInput = input()
+    preProcessor = DataPreProcessor(userInput)
 
     # Where output is the processed output.
     preProcessor.processInput()
     output = preProcessor.input
+    print(output)
 
 if __name__ == '__main__':
     main()
