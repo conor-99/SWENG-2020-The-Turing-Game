d51b976ff37388df6142668743e5f8f1ad946433
Leo <mccleale@tcd.ie>
2020-02-19 13:11:44 +0000

    added autocorrect and other

--- ai/chat_bot.py
+++ ai/chat_bot.py
@@ -1,17 +1,23 @@
-# Class worked on by: Claire, Luiz Fellipe, Leo 
+# Class worked on by: Claire, Luiz Fellipe, Leo
 from data_pre_processor import DataPreProcessor
 
 # Takes input from commandline, processes it, displays processed output via commandline.
 def main():
     # Where input is user input from commandline.
-    print("Enter the input")
-    userInput = input()
-    preProcessor = DataPreProcessor(userInput)
+    print('\033[1m' + "Enter the input, or enter 'exit' to end:" + '\033[0m')
+    while(1):
+        userInput = input()
+        # exit on 'exit'
+        if(userInput == "exit"):
+            break
+        preProcessor = DataPreProcessor(userInput)
 
-    # Where output is the processed output.
-    preProcessor.processInput()
-    output = preProcessor.input
-    print(output)
+        # Where output is the processed output.
+        preProcessor.processInput()
+        output = preProcessor.input
+        print('\033[94m' + "--> " + str(output) + '\033[0m')
+        print('\033[1m' + "Enter another input:" + '\033[0m')
+    print('\033[92m' + "Program exited." + '\033[0m')
 
 if __name__ == '__main__':
     main()
--- ai/data_pre_processor.py
+++ ai/data_pre_processor.py
@@ -1,22 +1,62 @@
 # Class worked on by: Claire, Diego, Kishore, Leo
-import re
+import re, os, hunspell
 from word2number import w2n
 from string import digits
 #from keras.preprocessing.text import text_to_word_sequence
 
 class DataPreProcessor:
+    spellchecker = hunspell.HunSpell("./dictionaries/en_GB.dic", "./dictionaries/en_GB.aff")
     input = 0
     # Creates an instance of the preprocessor with input being input from commandline passed to it from main class.
     def __init__(self, input):
         self.input = input
 
-    # Processes the input.
+    # Processes the input
     def processInput(self):
         self.convertAccentedCharsToAscii()
         self.removeNumbers()
         self.convertNumberWordToDigit()
-        #self.input = text_to_word_sequence(self.input)
+        # This is only relevant because of the weird library
+        self.input = str(self.input)
+        self.autoCorrect()
         return
+
+    # Rids the input of spelling mistakes, replacing with the most similar correctly spellled word
+    def autoCorrect(self):
+        #self.input = text_to_word_sequence(self.input)
+        array = self.string2Array(self.input)
+        for x in range(len(array)):
+            if(self.spellchecker.spell(array[x]) == False):
+                array[x] = self.spellchecker.suggest(array[x])[0]
+        self.input = self.array2String(array)
+        return
+
+    # Convert a ' ' delimited string to a list of words
+    # This will be deleted later, it is for testing purposes
+    def string2Array(self, string):
+        while(1 and len(string) > 0):
+            if(string[0] == " "):
+                string = string[1:len(string)]
+            else:
+                break
+        array = []
+        temp = ""
+        for x in string:
+            if(x != " "):
+                temp += x
+            else:
+                array.append(temp)
+                temp = ""
+        array.append(temp)
+        return array
+
+    # Convert an array of words back to a string
+    def array2String(self, array):
+        string = ""
+        for x in array:
+            string += x + " "
+        string = string[0:-1]
+        return string
 
     # Change accented characters eg. é to e. - Diego
     def convertAccentedCharsToAscii(self):
@@ -26,22 +66,22 @@ class DataPreProcessor:
         self.input = re.sub(r'[òóôõöðøōŏőÒÓÔÕÖØŌŎŐ]', 'o', self.input)
         self.input = re.sub(r'[ùúûüũūŭůűųÙÚÛÜŨŪŬŮŰŲ]', 'u', self.input)
         self.input = re.sub(r'[ýÿŷÝŸ]', 'y', self.input)
-        self.input = re.sub(r'[^A-Za-z0-9]+', '_', self.input)
         return
 
     # Convert one to 1. - Kishore
     def convertNumberWordToDigit(self):
         try:
             self.input = w2n.word_to_num(self.input)
+            print('\033[93m' + "The original input has been overwritten by this function." + '\033[0m')
         except Exception as e:
-            print(e)    
-        return 
+            return
+        return
 
     # Remove all numeric characters. - Kishore
     def removeNumbers(self):
         self.input = ''.join([i for i in self.input if not i.isdigit()])
         self.input = (self.input).replace("_", " ")
-        return 
+        return
 
     # Resolve words that have a '_' character
     # ie. if "h3llo" is passed through convertAccentedCharsToAscii(), it becomes "h_llo"
