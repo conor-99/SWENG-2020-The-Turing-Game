b25ebc76c0cd0da319600696724bf34132939e04
Luiz Fellipe Montibeller <60435968+lzfellipe@users.noreply.github.com>
2020-02-25 10:55:03 +0000

    Update test_data_pre_processor.py
    tests of functions

--- ai/test_data_pre_processor.py
+++ ai/test_data_pre_processor.py
@@ -1,14 +1,32 @@
 # Class worked on by: Claire, Diego, Luiz Fellipe
 import unittest
+from data_pre_processor import DataPreProcessor
 
 class TestDataPreProcessor(unittest.TestCase):
 
     # Test convert accented chars function - Diego 
-    def testConvertChar(self):
+   # def testConvertChar(self):
 
     # Test convert number word to digit - Luiz Fellipe
-    def testNumWordToDigit(self):
+    def test_NumWordToDigit(self):
+        testInput = 'one'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertNumberWordToDigit()
+        testInput = preProcessor.input
+        self.assertEqual(testInput,1)
+        
+        testInput = 'fsd'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertNumberWordToDigit()
+        testInput = preProcessor.input
+        self.assertEqual(testInput, 'fsd')
 
+        testInput = 'one.two'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertNumberWordToDigit()
+        testInput = preProcessor.input
+        print (f"{testInput} : testing input")
+        self.assertEqual(testInput, 1 + "." + 2)
 
 
 if __name__ == '__main__':
