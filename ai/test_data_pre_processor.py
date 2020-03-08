ab90d1889e1092d4c0ff90b028476d03f626f905
molamphd <60659482+molamphd@users.noreply.github.com>
2020-03-08 12:54:22 +0000

    Update test_data_pre_processor.py
    Test for covertAccentedCharsToAscii() function

--- ai/test_data_pre_processor.py
+++ ai/test_data_pre_processor.py
@@ -2,11 +2,48 @@
 import unittest
 from data_pre_processor import DataPreProcessor
 
-class TestDataPreProcessor(unittest.TestCase):
+class TestDataPreProcessor(unittest.TestCase):#pip3 install -U -r requirements.txt
 
     # Test convert accented chars function - Diego 
 
-    # def test_ConvertChar(self):
+       # def test_ConvertChar(self):  
+        testInput = 'æ'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput, 'a')
+
+        testInput = 'je ètais'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput, 'je etais')
+
+        testInput = 'í'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput, i) 
+
+        testInput = 'Ø'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput,'o') 
+       
+        testInput = 'Ùaemd'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput, 'uaemd') 
+
+        testInput = 'ůűų or ŷÝŸ'
+        preProcessor = DataPreProcessor(testInput)
+        preProcessor.convertAccentedCharsToAscii()
+        testInput = preProcessor.input
+        self.assertEqual(testInput,'uuu or yyy') 
+
+
 
     # Test convert number word to digit - Luiz Fellipe
     def test_NumWordToDigit(self):
