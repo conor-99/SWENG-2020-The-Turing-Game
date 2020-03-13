258132cd3c34c45ee248ba72f5d1552d9614e15d
leomcclean <47148527+leomcclean@users.noreply.github.com>
2020-03-13 15:25:47 +0000

    added terminal mode

--- ai/ai_brain.py
+++ ai/ai_brain.py
@@ -6,6 +6,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import tensorlayer as tl
 import numpy as np
+from data_pre_processor import DataPreProcessor
 from data.twitter import data
 from tensorlayer.cost import cross_entropy_seq, cross_entropy_seq_with_mask
 from tqdm import tqdm
@@ -37,7 +38,7 @@ class AI:
     # Creates an instance of the AI with it's name being passed from a name-generating function
     def __init__(self, name):
         self.name = name
-
+        self.preProcessor = DataPreProcessor(userInput)
         self.model_ = Seq2seq(
             decoder_seq_length = self.decoder_seq_length,
             cell_enc=tf.keras.layers.GRUCell,
@@ -50,11 +51,13 @@ class AI:
         load_weights = tl.files.load_npz(name="data/model.npz")
         tl.files.assign_weights(load_weights, self.model_)
 
+    # Handle the creation of a response from the given input
     def respond(self, seed, number):
+        sentence = self.simpleResponse(seed)
+
         self.model_.eval()
         seed_id = [self.word2idx.get(w, self.unk_id) for w in seed.split(" ")]
         sentence_id = self.model_(inputs=[[seed_id]], seq_length=20, start_token=self.start_id, top_n=number)
-        sentence = []
         for w_id in sentence_id[0]:
             w = self.idx2word[w_id]
             if w == 'end_id':
@@ -63,4 +66,14 @@ class AI:
 
         if sentence == []:
             sentence = ["I'm", "sorry,", "I", "just", "don't", "quite", "understand", "what", "you're", "asking..."]
+        return sentence
+
+    def simpleResponse(self, input):
+        sentence = []
+        input = self.preProcessor.string2Array(seed)
+        tally = [0, 0, 0]
+        greetings = ["hello","hi","greetings","hey"]
+
+        for x in input:
+            x = 0
         return sentence
--- ai/chat_bot.py
+++ ai/chat_bot.py
@@ -13,33 +13,33 @@ end = False
 
 # Takes input from commandline, processes it, displays processed output via commandline.
 def main():
-    # Set up our timer
-    timer = BackgroundScheduler()
-    timer.add_job(endProgram, "interval", minutes=1)
-    # Simple bit of beautifying for our command-line output
-    terminalColours = True
+    # This will be an environment variable later
+    terminalMode = True
+    # Simple bit of beautifying for our command-line output.
     c_background   = ""
-    c_blue      = ""
-    c_green     = ""
-    c_red       = ""
-    c_close     = ""
-    if terminalColours:
-        c_background   = "\033[44m"
-        c_blue      = "\033[94m"
-        c_green     = "\033[92m"
-        c_red       = "\033[91m"
-        c_close     = "\033[0m"
+    c_blue         = ""
+    c_green        = ""
+    c_red          = ""
+    c_close        = ""
+    # Certain aspects of this program will only exist on the commandline.
+    if terminalMode:
+        # Set up our timer
+        timer = BackgroundScheduler()
+        timer.add_job(endProgram, "interval", minutes=1)
+        c_background    = "\033[44m"
+        c_blue          = "\033[94m"
+        c_green         = "\033[92m"
+        c_red           = "\033[91m"
+        c_close         = "\033[0m"
+        # Small bit of starting text to introduce the user.
+        startingText = ["Ask a question, and see how our AI responds.",
+        "Please ensure queries have more than three words.",
+        "You will have 60 seconds upon entering your first query to make further queries.",
+        "The program will wait for your last query before exiting."]
+        print(c_green + "\n".join(startingText) + c_close)
 
-    # Small bit of starting text to introduce the user.
-    startingText = ["Ask a question, and see how our AI responds.",
-    "Please ensure queries have more than three words.",
-    "You will have 60 seconds upon entering your first query to make further queries.",
-    "The program will wait for your last query before exiting."]
-    
-    print(c_green + "\n".join(startingText) + c_close)
-
-    # Where input is user input from commandline.
-    print(c_background + "Enter the input, or enter 'exit' to end:" + c_close)
+        # Where input is user input from commandline.
+        print(c_background + "Enter the input, or enter 'exit' to end:" + c_close)
 
     personality = None
 
@@ -61,41 +61,41 @@ def main():
     initial = True
     global end
     while(not end):
-        userInput = input()
-        # Start the timer after the first user input
-        if initial:
-            timer.start()
-            initial = False
-        # Exit on 'exit'.
-        if(userInput == "exit"):
-            break
-        # Determine our input, whether an error or a valid query
-        output = arrangeResp(userInput, personality)
-        # Handle queries that are too short
-        if output == False:
-            print(c_red + "The input string must have a minimum of three words!" + c_close)
-        else:
-            print(c_blue + output + c_close)
-        print(c_background + "Enter another input:" + c_close)
-    print(c_green + "Program exited." + c_close)
+        if terminalMode:
+            userInput = input()
+            # Start the timer after the first user input.
+            if initial:
+                timer.start()
+                initial = False
+            # Exit on 'exit'.
+            if(userInput == "exit"):
+                break
+            # Determine our input, whether an error or a valid query.
+            output = arrangeResp(userInput, personality)
+            # Handle queries that are too short.
+            if output == False:
+                print(c_red + "The input string must have a minimum of three words!" + c_close)
+            else:
+                print(c_blue + output + c_close)
+            print(c_background + "Enter another input:" + c_close)
+    if terminalMode:
+        print(c_green + "Program exited." + c_close)
 
 def arrangeResp(userInput, personality):
-    #Creating our AI
+    # Creating our AI.
     ai = AI(personality.name)
     # Where output is the processed output.
     preProcessor = DataPreProcessor(userInput)
-    # Was the query too short for the AI, if so, exit
+    # Was the query too short for the AI, if so, exit.
     if preProcessor.processInput() == False:
         return False
     # This is passed to the model.
     processedInput = preProcessor.input
-    # Receive response from the model
+    # Receive response from the model.
     response = ai.respond(processedInput, 1)
     response = preProcessor.array2String(response)
-    # postProcessor = DataPostProcessor(response, personality)
-    postProcessor = DataPostProcessor(processedInput, personality)
+    postProcessor = DataPostProcessor(response, personality)
     postProcessor.postProcess()
-
     return response
 
 def endProgram():
