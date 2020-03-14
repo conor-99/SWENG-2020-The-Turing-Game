e0b1c56406310156bc09e4beb511c9065d683c08
molamphd <60659482+molamphd@users.noreply.github.com>
2020-03-14 14:11:00 +0000

    Update adult.py
    Added basic functionality to addPunctuation function

--- ai/adult.py
+++ ai/adult.py
@@ -16,4 +16,17 @@ class Adult(Personality):
     # The returned string from the AI model does not have any punctuation. 
     # This function needs to add punction to that string and return it. - Diego
     def addPunctuation(self, input):
-        return input
+       # input = "hello how are you i cant hear you and i cant see"
+        input = input + '.'
+        input = input.replace('cant', 'can^t')
+        input = input.replace('hello', 'hello!')
+        input = input.replace('isnt', 'isn^t')
+        input = input.replace('would', 'wouldn^t')
+        input = input.replace('hadnt', 'hadn^t')
+        input = input.replace('how are you', 'how are you?')
+        input = input.replace('where are you', 'where are you?')
+        input = input.replace('are you going?', 'how are you?')
+        input = input.replace('however', 'however,')
+       # print(input)
+
+	
