5805cc51f161efd52f1d8d6af00e5598cf86a28d
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-03-15 20:13:52 +0000

    Added the reworked backend folder

---
+++ backend/src/backend/main.java
@@ -0,0 +1,31 @@
+package backend;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+//FEEDBACK UPLOAD PART
+
+//private EditText mContactEmail;
+//private EditText mMessage;
+//
+//mContactEmail = findViewById(R.id.contactEmail);
+//mMessage = findViewById(R.id.contactMessage);
+//private void uploadContactMessage(){
+//    String message = mMessage.getText().toString().trim();
+//    String email = mContactEmail.getText().toString().trim();
+//
+//    FirebaseAuth Auth = FirebaseAuth.getInstance();
+//    FirebaseDatabase Database = FirebaseDatabase.getInstance();
+//    DatabaseReference DatabaseRefFeedback = Database.getReference("feedback");
+//    DatabaseReference newFeedback = DatabaseRefFeedback.push();
+//    newFeedback.child("message").setValue(message);
+//    newFeedback.child("reply-email").setValue(email);
+//
+//
+//}
+
