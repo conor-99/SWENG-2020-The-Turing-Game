689eb064c9fa6bb637a790b096263912ae74305e
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-03-16 11:14:36 +0000

    Made a gradle build

--- backend/src/main/java/backend/MainActivity.java
+++ backend/src/main/java/backend/MainActivity.java
@@ -1,9 +1,55 @@
 package backend;
 
-public class main {
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity {
 
 	public static void main(String[] args) {
-		
+		try {
+			authenticateServer();
+			System.out.println("Admin server authenticated!");
+		} catch(Exception e) {
+			System.out.println("Failed authenticating the server");
+			e.printStackTrace();
+		}	
+		while(true) {}
+	}
+	
+	private static void authenticateServer() throws IOException {
+		// Fetch the service account key JSON file contents
+		FileInputStream serviceAccount = new FileInputStream("src/main/resources/turing-game-e5059-firebase-adminsdk-eata7-4d6eb69ec9.json");
+
+		// Initialize the app with a service account, granting admin privileges
+		FirebaseOptions options = new FirebaseOptions.Builder()
+		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		    .setDatabaseUrl("https://turing-game-e5059.firebaseio.com")
+		    .build();
+		FirebaseApp.initializeApp(options);
+
+		// As an admin, the app has access to read and write all data, regardless of Security Rules
+		DatabaseReference ref = FirebaseDatabase.getInstance()
+		    .getReference("restricted_access/secret_document");
+		ref.addListenerForSingleValueEvent(new ValueEventListener() {
+		  @Override
+		  public void onDataChange(DataSnapshot dataSnapshot) {
+		    Object document = dataSnapshot.getValue();
+		    System.out.println(document);
+		  }
+
+		  @Override
+		  public void onCancelled(DatabaseError error) {
+		  }
+		});
 	}
 
 }
