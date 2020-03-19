cfbbc2106cfb47f27760b3c3fac8c034eca6acfa
Hailing <hailing45@icloud.com>
2020-03-19 08:36:46 +0000

    generate variables

--- backend/src/main/java/backend/User.java
+++ backend/src/main/java/backend/User.java
@@ -1,7 +1,18 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 public class User
 {
-	
+	int rank;
+	String username;
+	Double score;
+
 }
 
