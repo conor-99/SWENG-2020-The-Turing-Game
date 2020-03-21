6dc5d6193cfc3f0d04d0a287e94df4cc139fbc10
conormccauley1999 <conormccauley1999@gmail.com>
2020-03-21 13:08:16 +0000

    Merged click listeners and add example API call

--- client/TheTuringGame/app/src/main/java/com/sweng/theturinggame/SurveyActivity.java
+++ client/TheTuringGame/app/src/main/java/com/sweng/theturinggame/SurveyActivity.java
@@ -12,13 +12,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class SurveyActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
         // initiate rating bar and a button
         final RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.ratingBar);
         Button submitButton = (Button) findViewById(R.id.button);
+
         // perform click event on button
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,18 +32,14 @@ public class SurveyActivity extends AppCompatActivity {
                 String rating = "Rating :: " + simpleRatingBar.getRating();
                 //was used to test if the rating was correct
                 //Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
-            }
-        });
-        //when 'Submit' button clicked...
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
+
                 startActivity(new Intent(SurveyActivity.this, MainActivity.class));
+
+                API.submitSurveyResults(5, "test");
+
             }
         });
 
     }
-
-    //cant find API.submitSurveyResults() method.
 
 }
