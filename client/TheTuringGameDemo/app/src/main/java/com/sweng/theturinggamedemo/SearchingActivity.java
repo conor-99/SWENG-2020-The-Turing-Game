fb3bed75e32d97969e2c9d0262a6b716377b3921
conormccauley1999 <conormccauley1999@gmail.com>
2020-03-22 20:18:40 +0000

    Added empty activities

---
+++ client/TheTuringGameDemo/app/src/main/java/com/sweng/theturinggamedemo/SearchingActivity.java
@@ -0,0 +1,16 @@
+package com.sweng.theturinggamedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching);
+
+    }
+
+}
